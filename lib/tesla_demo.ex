ff4b46ce8e9e073fef6f07a3fda0cdd8cf9c4ac5
LindaKadz <achielinda44@gmail.com>
2019-09-13 20:30:16 -0700

    rectify header format, use list instead of map use get request to get issues from a users repo

--- lib/tesla_demo.ex
+++ lib/tesla_demo.ex
@@ -6,7 +6,21 @@ defmodule TeslaDemo do
   use Tesla
 
   plug Tesla.Middleware.BaseUrl, "https://api.github.com"
-  plug Tesla.Middleware.Headers, %{"User-Agent" => "tesla"}
+  plug Tesla.Middleware.Headers, [{"user-agent", "Tesla"}]
   plug Tesla.Middleware.JSON
+
+  @doc """
+  Gets all the issues in a selected repo of the user, this uses the GET request.
+
+  ## Examples
+
+      iex> TeslaDemo.issues("Podiihq", "nailinda")
+      :world
+
+  """
+
+  def issues(user, project) do
+    get("https://api.github.com/repos/#{user}/#{project}/issues")
+  end
 
 end
